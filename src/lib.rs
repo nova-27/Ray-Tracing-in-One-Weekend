80ae0e57f6fa1576bf7fa0143db5fbf6310e0352
nova27 <33594303+nova-27@users.noreply.github.com>
2024-11-07 03:43:59 +0900

    1.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,145 @@
+use std::ops::{Add, Div, Mul, Sub};
+
 pub struct Color {
-    r: f64,
-    g: f64,
-    b: f64,
+    pub r: f64,
+    pub g: f64,
+    pub b: f64,
 }
 
 impl Color {
-    pub fn new(r: f64, g: f64, b: f64) -> Self {
-        Self { r, g, b }
-    }
-
     pub fn write_color(&self) {
         let r = (244.999 * self.r) as i32;
         let g = (244.999 * self.g) as i32;
         let b = (244.999 * self.b) as i32;
         println!("{} {} {}", r, g, b);
     }
+}
+
+impl Add<Color> for Color {
+    type Output = Self;
+
+    fn add(self, rhs: Color) -> Self::Output {
+        Self {
+            r: self.r + rhs.r,
+            g: self.g + rhs.g,
+            b: self.b + rhs.b,
+        }
+    }
+}
+
+impl Mul<Color> for f64 {
+    type Output = Color;
+
+    fn mul(self, rhs: Color) -> Self::Output {
+        Color {
+            r: self * rhs.r,
+            g: self * rhs.g,
+            b: self * rhs.b,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Point3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Add<Vec3> for Point3 {
+    type Output = Self;
+
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Sub<Vec3> for Point3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl Sub<Point3> for Point3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Point3) -> Self::Output {
+        Vec3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vec3 {
+    fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn unit_vector(self) -> Self {
+        self / self.length()
+    }
+}
+
+impl Add<Vec3> for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
+pub struct Ray {
+    pub origin: Point3,
+    pub direction: Vec3,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,74 @@
-use ray_tracing_in_one_weekend::Color;
+use ray_tracing_in_one_weekend::{Color, Point3, Ray, Vec3};
 
 fn main() {
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 384;
+    const IMAGE_HEIGHT: i32 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as i32;
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
     println!("255");
 
+    const VIEWPORT_HEIGHT: f64 = 2.0;
+    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f64 = 1.0;
+
+    const ORIGIN: Point3 = Point3 {
+        x: 0.,
+        y: 0.,
+        z: 0.,
+    };
+    const HORIZONTAL: Vec3 = Vec3 {
+        x: VIEWPORT_WIDTH,
+        y: 0.,
+        z: 0.,
+    };
+    const VERTICAL: Vec3 = Vec3 {
+        x: 0.,
+        y: VIEWPORT_HEIGHT,
+        z: 0.,
+    };
+
+    let lower_left_corner: Point3 = ORIGIN
+        - HORIZONTAL / 2.
+        - VERTICAL / 2.
+        - Vec3 {
+            x: 0.,
+            y: 0.,
+            z: FOCAL_LENGTH,
+        };
+
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
-                (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
-                0.25,
-            );
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
 
+            let ray = Ray {
+                origin: ORIGIN,
+                direction: lower_left_corner - ORIGIN + u * HORIZONTAL + v * VERTICAL,
+            };
+
+            let pixel_color = ray_color(&ray);
             pixel_color.write_color();
         }
     }
 
     eprintln!("Done.")
+}
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction.unit_vector();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t)
+        * Color {
+            r: 1.0,
+            g: 1.0,
+            b: 1.0,
+        }
+        + t * Color {
+            r: 0.5,
+            g: 0.7,
+            b: 1.0,
+        }
 }
