b687d39fdc4efca8acbc0e56df906c61196bc61b
nova27 <33594303+nova-27@users.noreply.github.com>
2024-11-07 02:08:23 +0900

    1.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_WIDTH: usize = 256;
+    const IMAGE_HEIGHT: usize = 256;
+
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remaining: {}", j);
+        for i in 0..IMAGE_WIDTH {
+            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+            let b = 0.25;
+
+            let r = (255.999 * r) as usize;
+            let g = (255.999 * g) as usize;
+            let b = (255.999 * b) as usize;
+
+            println!("{} {} {}", r, g, b);
+        }
+    }
+
+    eprintln!("Done.")
 }
